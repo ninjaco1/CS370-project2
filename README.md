317e2ae55128a93ee5226da1b929f9642d616b0f
Anthony Nguyen <nguyean6@flip2.engr.oregonstate.edu>
2020-11-24 13:42:33 -0800

    create main

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CS370-project2
+
+The program `submission.py` is writen in python
+
+To generate the QR code the following command must be typed: `python submission.py --generate-qr`.
+To get the OTP code the following command must be typed: `python submission.py --get-otp`.
--- submission.py
+++ submission.py
@@ -1,12 +1,25 @@
 import pyotp # version 2.3.0
 import pyqrcode
+import sys
+from time import sleep
+
 
 
 # generate QR code
 def generateqr(data):
     qr = pyqrcode.create(data)
-    qr.png('abc.png',scale=8)
+    qr.png('abca.png',scale=8)
 
-link = pyotp.totp.TOTP('JBSWY3DPEHPK3PXP').provisioning_uri(name='tester@google.com', issuer_name='Secure App')
 
-generateqr(link)
+# main
+if __name__ == "__main__":
+    if sys.argv[1] == "--generate-qr": # generate QR
+        # link for generating the TOTP
+        # having the same TOTP will result in the same key
+        link = pyotp.totp.TOTP('JBSWY3DPEHPK3PXP').provisioning_uri(name='testers@google.com', issuer_name='Secure App')
+        print link
+        generateqr(link)
+
+    if sys.argv[1] == "--get-otp": # number of the QR
+        totp = pyotp.TOTP("JBSWY3DPEHPK3PXP")
+        print("Current OTP:", totp.now())
